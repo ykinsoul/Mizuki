51a84739eaedf1214b6c6d41c417a244f070f4d1
ysl <2930390085@qq.com>
2026-07-29 23:08:08 +0800

    1

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -36,9 +36,9 @@ export const siteConfig: SiteConfig = {
 		// 顶栏标题文本
 		text: "等嗡吧",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.webp
-		icon: "assets/home/home.webp",
+		icon: "assets/home/home.png",
 		// 网站Logo图片路径
-		logo: "assets/home/default-logo.webp",
+		logo: "assets/home/default-logo.png",
 	},
 
 	// 页面自动缩放配置
