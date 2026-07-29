f3ed274b29eba5d74e288ebaa332d368916648b3
ysl <2930390085@qq.com>
2026-07-30 00:22:39 +0800

    last change

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -109,16 +109,10 @@ export const siteConfig: SiteConfig = {
 		// 支持单张图片或图片数组，当数组长度 > 1 时自动启用轮播
 		src: {
 			desktop: [
-				"/assets/desktop-banner/1.webp",
-				"/assets/desktop-banner/2.webp",
-				"/assets/desktop-banner/3.webp",
-				"/assets/desktop-banner/4.webp",
+				"/assets/desktop-banner/logo.png",
 			], // 桌面横幅图片
 			mobile: [
-				"/assets/mobile-banner/1.webp",
-				"/assets/mobile-banner/2.webp",
-				"/assets/mobile-banner/3.webp",
-				"/assets/mobile-banner/4.webp",
+				"/assets/desktop-banner/logo.png",
 			], // 移动横幅图片
 		}, // 使用本地横幅图片
 
