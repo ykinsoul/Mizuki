ccf8ddeed6b38429675dd606d3831e096b5c55b1
ysl <2930390085@qq.com>
2026-07-30 01:00:27 +0800

    renew URL

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 export const siteConfig: SiteConfig = {
 	title: "FTAS",
 	subtitle: "创造你自己，，，",
-	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://www.dengweng8.icu/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-07-29", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	lang: SITE_LANG,
